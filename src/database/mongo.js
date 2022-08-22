06ea78cee2c96358a1689ce40789944b6e59f7e5
RobertMario <111745185+RobertMario@users.noreply.github.com>
2022-08-22 13:43:53 +0300

    Add files via upload

---
+++ src/database/mongo.js
@@ -0,0 +1,27 @@
+// ./src/database/mongo.js
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const { MongoClient } = require('mongodb');
+
+
+let database = null;
+
+async function startDatabase() {
+    const mongo = new MongoMemoryServer();
+    const mongoDBURL = await mongo.getConnectionString();
+    const connection = await MongoClient.connect(mongoDBURL, { useNewUrlParser: true });
+    database = connection.db();
+
+}
+startDatabase().then(async() => {
+    await insertAd({ title: 'Hello, now from the in-memory database!' });
+})
+
+async function getDatabase() {
+    if (!database) await startDatabase();
+    return database;
+}
+
+module.exports = {
+    getDatabase,
+    startDatabase,
+};
